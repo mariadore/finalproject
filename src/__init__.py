b67ab5f0ab88886ff39355c26a42fefbc053f520
Brielle Natzic <bnatzic@umich.edu>
2025-12-07 14:16:43 -0500

    Recreate main.py and update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,15 @@
-from .fetch_crime import get_crime_data
-from .fetch_weather import get_weather_data
+from .fetch_crime import fetch_and_store_crimes, get_crime_data
+from .fetch_weather import fetch_weather_for_all_locations, get_weather_data
+from .fetch_geocode import geocode_and_attach_locations
+from .process_and_visualize import run_visualizations
+from .db_utils import set_up_database
 
 __all__ = [
     "get_crime_data",
     "get_weather_data",
+    "fetch_and_store_crimes",
+    "fetch_weather_for_all_locations",
+    "geocode_and_attach_locations",
+    "run_visualizations",
+    "set_up_database"
 ]
