0194aa2d3d095cad45c8443dfc8def115cc6007d
Brielle Natzic <bnatzic@umich.edu>
2025-12-10 20:11:36 -0500

    debugging

--- src/__init__.py
+++ src/__init__.py
@@ -2,12 +2,10 @@ from .fetch_crime import fetch_and_store_crimes
 from .fetch_weather import fetch_weather_for_all_locations
 from .fetch_geocode import geocode_and_attach_locations
 from .db_utils import set_up_database
-from .visualizations import run_visualizations   
 
 __all__ = [
     "fetch_and_store_crimes",
     "fetch_weather_for_all_locations",
     "geocode_and_attach_locations",
-    "set_up_database",
-    "run_visualizations"
+    "set_up_database"
 ]
